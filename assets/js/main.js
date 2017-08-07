9225b4d9de82c3443f91287299379151d55c4d1e
Anayn Pavez <nanyn.pavez@gmail.com>
2017-08-07 06:21:56 -0300

    cambio de navbar al hacer scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//animación del logo
 var paths = document.querySelectorAll('svg path');
 for(i = 0; i < paths.length; i++){
 	paths[i].setAttribute('stroke-dasharray', paths[i].getTotalLength());
@@ -13,8 +14,14 @@ $(document).ready(function() {
 		console.log();
 		if (($("#menu-ppal").offset().top - $(document).scrollTop() <= 0) && ($("#Capa_1").height() - $("#Capa_1").offset().top - $(document).scrollTop() <= 0)) {
         	$("#menu-ppal").addClass("fixed");
+        	$("#menu-ppal").css('background-color', '#758918');
+        	$("#menu-ppal").css('box-shadow', '0 0 0.3em #000');
         } else {
-            $("#menu-ppal").removeClass("fixed");
+            $("#menu-ppal").css('background-color', 'transparent');
+            $("#menu-ppal").css('box-shadow', 'none');
         }
     });
+
+    $(".button-collapse").sideNav();
+
 });
